2e059d2407a93ddf2d80ac7955b5e5676789667f
Jeffrey <j.lo128456@gmail.com>
2025-04-16 20:46:33 +1000

    completed Assigment

---
+++ migrations/versions/961403971705_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 961403971705
+Revises: 
+Create Date: 2025-04-16 20:09:15.027402
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '961403971705'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
---
+++ migrations/versions/fce7fbd1b926_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: fce7fbd1b926
+Revises: 961403971705
+Create Date: 2025-04-16 20:32:07.912591
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'fce7fbd1b926'
+down_revision = '961403971705'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
